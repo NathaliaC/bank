2227675332dd99604199ad76811c210e9e9731d5
Nathalia Chaves <nathalia.chaves2@gmail.com>
2020-04-15 00:48:16 -0300

    User structure created

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+User.create!(name: 'Nathalia', email: 'nathalia@gmail.com', password: '123456', password_confirmation: '123456')
+User.create!(name: 'Daniel', email: 'daniel@gmail.com', password: '987654', password_confirmation: '987654')
+User.create!(name: 'Ronaldo', email: 'ronaldo@gmail.com', password: 'qwe123', password_confirmation: 'qwe123')
